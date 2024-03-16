6c9cfbefa34240ded10c589e4a54a9668c37d83b
swarnim.gupta <swarnim.gupta@engati.com>
2024-03-16 19:58:29 +0530

    remove transactional

--- src/main/java/com/qp/qpassessment/service/impl/UserServiceImpl.java
+++ src/main/java/com/qp/qpassessment/service/impl/UserServiceImpl.java
@@ -42,7 +42,6 @@ public class UserServiceImpl implements UserService {
   }
 
   @Override
-  @Transactional
   public UserOrder placeProductsOrder(String username, List<GroceryItemOrderDTO> groceryItemList)
       throws MyCustomException {
     List<GroceryItemInventory> itemInventories =
